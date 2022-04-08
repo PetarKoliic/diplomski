e7b5471ecdd2d777f93d9a331acc2ba807c7ce52
Petar Kolic <kp160159d@student.etf.bg.ac.rs>
2022-04-08 08:12:43 +0200

    enkapsuliranje metoda u novu klasu topic-paginacija ne radi sve

--- frontend/src/app/forum/forum.component.ts
+++ frontend/src/app/forum/forum.component.ts
@@ -7,6 +7,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { PageEvent } from '@angular/material/paginator';
 import { NotificationService } from '../services/notification.service';
 import { ChangeDetectorRef } from '@angular/core';
+import { TopicPagination } from '../models/topic-pagination';
 
 
 @Component({
@@ -22,9 +23,13 @@ export class ForumComponent implements OnInit {
 
   topics: Topic[];
   topics_social: Topic[] = [];
-  topics_art_piece: Topic[] = [];
+  
   topics_fair: Topic[] = [];
 
+
+  // topics_art_piece: Topic[] = [];
+  // show_topics_art_piece: Topic[];
+  arts: TopicPagination = new TopicPagination();
 
 
   search_topic: string;
@@ -37,30 +42,31 @@ export class ForumComponent implements OnInit {
     console.log(this.up_down_flag);
     console.log(this.sort_choosen);
 
-    this.show_topics_art_piece.sort((a: Topic, b: Topic) => {
+    this.arts.sort(this.search_topic, this.sort_choosen, this.up_down_flag);
+    // this.show_topics_art_piece.sort((a: Topic, b: Topic) => {
 
 
-      let flag = this.up_down_flag === "up" ? 1 : -1;
+    //   let flag = this.up_down_flag === "up" ? 1 : -1;
 
 
-      // if (this.sort_choosen === "date_created")
-      //   return a.title > b.title ? 1 * flag : -1 * flag;
+    //   // if (this.sort_choosen === "date_created")
+    //   //   return a.title > b.title ? 1 * flag : -1 * flag;
 
-      if (this.sort_choosen === "title" || this.sort_choosen === "views" ||
-        this.sort_choosen === "date_added")
-        return a[this.sort_choosen] > b[this.sort_choosen] ? 1 * flag : -1 * flag;
-      else if (this.sort_choosen === "comments_count")
-        return a.comments.length - b.comments.length;
-      else // last comment
-        return a.comments[a.comments.length - 1].date_added > b.comments[b.comments.length - 1].date_added ? 1 * flag : -1 * flag;
+    //   if (this.sort_choosen === "title" || this.sort_choosen === "views" ||
+    //     this.sort_choosen === "date_added")
+    //     return a[this.sort_choosen] > b[this.sort_choosen] ? 1 * flag : -1 * flag;
+    //   else if (this.sort_choosen === "comments_count")
+    //     return a.comments.length - b.comments.length;
+    //   else // last comment
+    //     return a.comments[a.comments.length - 1].date_added > b.comments[b.comments.length - 1].date_added ? 1 * flag : -1 * flag;
 
-    });
+    // });
 
-    this.sort_array_criteria(this.show_topics_art_piece);
-    this.sort_array_criteria(this.topics_art_piece);
+    // this.sort_array_criteria(this.show_topics_art_piece);
+    // this.sort_array_criteria(this.topics_art_piece);
 
-    console.log(this.show_topics_art_piece);
-    console.log(this.topics_art_piece);
+    // console.log(this.show_topics_art_piece);
+    // console.log(this.topics_art_piece);
 
   }
 
@@ -100,7 +106,8 @@ export class ForumComponent implements OnInit {
 
       this.topics = topics;
 
-      this.topics_art_piece = [];
+      // this.topics_art_piece = [];
+      this.arts.topics = []
       this.topics_fair = [];
       this.topics_social = [];
 
@@ -114,7 +121,8 @@ export class ForumComponent implements OnInit {
           this.topics_fair.push(this.topics[i]);
         }
         else if (this.topics[i].category === "art") {
-          this.topics_art_piece.push(this.topics[i]);
+          // this.topics_art_piece.push(this.topics[i]);
+          this.arts.topics.push(this.topics[i]);
 
           // console.log(i);
           // console.log("jer sam usao ovde");
@@ -123,13 +131,15 @@ export class ForumComponent implements OnInit {
         }
       }
       console.log("dosli dovde");
-      console.log(this.topics_art_piece);
-
-      this.topics_art_piece = this.topics_art_piece.filter(topic => topic.title.includes(this.search_topic));
-      this.show_topics_art_piece = this.topics_art_piece;
+      // console.log(this.topics_art_piece);
+      console.log(this.arts.topics);
+      
+      // this.topics_art_piece = this.topics_art_piece.filter(topic => topic.title.includes(this.search_topic));
+      // this.show_topics_art_piece = this.topics_art_piece;
+      this.arts.topics = this.arts.topics.filter(topic => topic.title.includes(this.search_topic));
 
       console.log("art piece");
-      console.log(this.topics_art_piece);
+      console.log(this.arts.topics);
     });
 
 
@@ -158,23 +168,31 @@ export class ForumComponent implements OnInit {
   endIndex: number = 1;
   startIndex : number = 0;
 
-  paginator_images: Array<String[]>;
-  show_topics_art_piece: Topic[];
+  // paginator_images: Array<String[]>;
   pageEvent(event: PageEvent) {
     this.startIndex = event.pageIndex * event.pageSize;
     this.endIndex = this.startIndex + event.pageSize;
 
-    this.show_topics_art_piece = [];
+    // this.show_topics_art_piece = [];
+    this.arts.show_topics = [];
 
-    if (this.endIndex > this.topics_art_piece.length)
-      this.endIndex = this.topics_art_piece.length;
+    // if (this.endIndex == this.topics_art_piece.length)
+    //   this.endIndex = this.topics_art_piece.length;
+
+    if (this.endIndex == this.arts.topics.length)
+      this.endIndex = this.arts.topics.length;
+
+
+    this.arts.fill_indexes(this.startIndex, this.endIndex, event.pageSize);
 
     // ovo izmeni
     // this.paginator_images=this.all_countries.slice(startIndex,endIndex);
 
     // this.img_pagination.set(appraisal._id, { "start_index": startIndex, "end_index": endIndex });
 
-    this.show_topics_art_piece = this.topics_art_piece.slice(this.startIndex, this.endIndex);
+
+    this.arts.refresh_show_topics();
+    // this.show_topics_art_piece = this.topics_art_piece.slice(this.startIndex, this.endIndex);
 
   }
 
@@ -187,7 +205,7 @@ export class ForumComponent implements OnInit {
   load_topics() {
 
 
-    this.show_topics_art_piece = [];
+    // this.show_topics_art_piece = [];
 
 
     this.service.get_all_topics().subscribe((topics: Topic[]) => {
@@ -196,7 +214,7 @@ export class ForumComponent implements OnInit {
 
       this.topics = topics;
 
-      this.topics_art_piece = [];
+      // this.topics_art_piece = [];
       this.topics_fair = [];
       this.topics_social = [];
 
@@ -211,7 +229,9 @@ export class ForumComponent implements OnInit {
           this.topics_fair.push(this.topics[i]);
         }
         else if (this.topics[i].category === "art") {
-          this.topics_art_piece.push(this.topics[i]);
+          // this.topics_art_piece.push(this.topics[i]);
+
+          this.arts.topics.push(this.topics[i]);
 
           console.log(i);
           console.log("jer sam usao ovde");
@@ -221,15 +241,21 @@ export class ForumComponent implements OnInit {
       }
 
 
-      console.log("duzina niza" + this.topics_art_piece.length);
-      if (this.endIndex + 1 <= this.topics_art_piece.length)
-        this.endIndex++;
+      // console.log("duzina niza" + this.topics_art_piece.length);
+      console.log("duzina niza" + this.arts.topics.length);
 
-    this.show_topics_art_piece = [...this.topics_art_piece.slice(this.startIndex, this.endIndex)];
+      // ???????????????????????????????????????
+      // if (this.endIndex + 1 == this.topics_art_piece.length)
+      //   this.endIndex++;
+      // ???????????????????????????????????????
+
+    // this.show_topics_art_piece = [...this.topics_art_piece.slice(this.startIndex, this.endIndex)];
+      this.arts.refresh_show_topics();
 
     this.changeDetectorRefs.detectChanges();      
       console.log("load_topics()");
-      console.log(this.show_topics_art_piece);
+      // console.log(this.show_topics_art_piece);
+      console.log(this.arts.show_topics);
     });
   }
 
---
+++ frontend/src/app/models/topic-pagination.ts
@@ -0,0 +1,88 @@
+import { Appraisal } from "./appraisal.model";
+import { Topic } from "./topic.model";
+
+
+export class TopicPagination {
+
+    startIndex: number;
+    endIndex: number;
+    pageSize: number;
+    topics: Topic[] = [];
+    show_topics: Topic[] = []
+
+
+    constructor(startIndex: number = 0, endIndex: number = 0, pageSize: number = 0) {
+
+        this.fill_indexes(startIndex, endIndex, pageSize);
+    }
+
+    fill_indexes(startIndex: number, endIndex: number, pageSize: number) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        this.pageSize = pageSize;
+    }
+
+    sort(search_topic: string,sort_choosen: string ,up_down_flag:string) {
+        console.log(up_down_flag);
+        console.log(sort_choosen);
+    
+        this.topics.sort((a: Topic, b: Topic) => {
+    
+    
+          let flag = up_down_flag === "up" ? 1 : -1;
+    
+    
+          // if (this.sort_choosen === "date_created")
+          //   return a.title > b.title ? 1 * flag : -1 * flag;
+    
+          if (sort_choosen === "title" || sort_choosen === "views" ||
+            sort_choosen === "date_added")
+            return a[sort_choosen] > b[sort_choosen] ? 1 * flag : -1 * flag;
+          else if (sort_choosen === "comments_count")
+            return a.comments.length - b.comments.length;
+          else // last comment
+            return a.comments[a.comments.length - 1].date_added > b.comments[b.comments.length - 1].date_added ? 1 * flag : -1 * flag;
+    
+        });
+    
+        TopicPagination.sort_array_criteria(this.show_topics, search_topic, sort_choosen, up_down_flag);
+        TopicPagination.sort_array_criteria(this.topics, search_topic, sort_choosen, up_down_flag);
+    
+        console.log(this.show_topics);
+        console.log(this.topics);
+    
+      }
+
+  static sort_array_criteria(topics: Topic[], search_topic: string,sort_choosen: string ,up_down_flag:string)
+  {
+    topics.sort((a: Topic, b: Topic) => {
+
+
+      let flag = up_down_flag === "up" ? 1 : -1;
+
+
+      // if (this.sort_choosen === "date_created")
+      //   return a.title > b.title ? 1 * flag : -1 * flag;
+
+      if (sort_choosen === "title" || sort_choosen === "views" ||
+        sort_choosen === "date_added")
+        return a[sort_choosen] > b[sort_choosen] ? 1 * flag : -1 * flag;
+      else if (sort_choosen === "comments_count")
+        return a.comments.length - b.comments.length;
+      else // last comment
+        return a.comments[a.comments.length - 1].date_added > b.comments[b.comments.length - 1].date_added ? 1 * flag : -1 * flag;
+
+    });
+
+    console.log(topics);
+  }
+
+  refresh_show_topics()
+  {
+    this.show_topics = [...this.topics.slice(this.startIndex, this.endIndex)];
+  }
+
+
+
+
+}
